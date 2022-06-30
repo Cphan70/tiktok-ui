dca3def3cea4d6cd24709721b785ca7aefe5a200
cphan70 <vanchiphan70@gmail.com>
2022-06-30 10:44:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from './pages/Home'
+import News from './pages/News'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
