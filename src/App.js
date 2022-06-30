f502d5c0f42cdb0ee1b4592a1c1157b62d2b0f5c
cphan70 <vanchiphan70@gmail.com>
2022-06-30 16:58:32 +0700

    cấu hình Router/ Layout , đưa cấu hình routes ra ngoài xây dựng cơ chế tải Layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
-
-import Home from './pages/Home'
-import News from './pages/News'
-import Contact from './pages/Contact'
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
